b460fb1413f355b0eff2dcbeb4b987b3b86e0e75
aegis-katoh <33014613+aegis-katoh@users.noreply.github.com>
2017-11-20 15:59:24 +0900

    no message

--- RaspberryPi/Analysis/Analyse_Pulse_Signal/main_Ver.2.py
+++ RaspberryPi/Analysis/Analyse_Pulse_Signal/main_Ver.2.py
@@ -61,9 +61,9 @@ def count_edge(edge, count):
 def calc_timedelta(standard_time, sampling_period):
 	current_time = datetime.now()
 	dif = current_time - standard_time
-	print(standard_time, current_time, dif)
+	print(standard_time)
+	print(current_time, dif)
 	timedelta = (dif % sampling_period).total_seconds()
-	print(timedelta)
 
 	return timedelta
 
