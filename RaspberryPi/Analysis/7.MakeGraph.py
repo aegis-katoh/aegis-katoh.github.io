4622a0fccdef32930fe24041b73eb74cf79dbe47
aegis-katoh <aegis.katoh@gmail.com>
2017-11-22 10:47:26 +0900

    make it possible

--- RaspberryPi/Analysis/7.MakeGraph.py
+++ RaspberryPi/Analysis/7.MakeGraph.py
@@ -1,6 +1,7 @@
 # This program is written in Python3
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 logfile = "Analyse_Pulse_Signal/20171121_log.csv"
@@ -10,7 +11,21 @@ data = pd.read_csv(logfile, parse_dates=[0])
 time = data.Time
 rate = data.Rate
 
-print(time, rate)
+print(time)
 
-plt.plot(time, rate)
+average = rate.mean()
+
+length = len(data.index)
+
+mean = pd.DataFrame(np.arange(length).reshape(length,1))
+mean.columns=["Mean"]
+
+for i in range(0, length):
+	mean["Mean"][i] = average
+
+print(mean)
+
+ax = plt.subplot()
+ax.plot(time, rate, time, mean)
+#ax.set_xlim([time[0]], time[length-1])
 plt.savefig("graph.png")
